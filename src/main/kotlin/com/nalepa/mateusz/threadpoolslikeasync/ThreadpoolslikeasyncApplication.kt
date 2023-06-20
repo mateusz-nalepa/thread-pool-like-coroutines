fa28982f159b14654bb57c506c09bdd5c85f7ec4
Mateusz Nalepa <mateuszcholyn@gmail.com>
2023-06-20 22:47:01 +0200

    init

---
+++ src/main/kotlin/com/nalepa/mateusz/threadpoolslikeasync/ThreadpoolslikeasyncApplication.kt
@@ -0,0 +1,11 @@
+package com.nalepa.mateusz.threadpoolslikeasync
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ThreadpoolslikeasyncApplication
+
+fun main(args: Array<String>) {
+	runApplication<ThreadpoolslikeasyncApplication>(*args)
+}
---
+++ src/test/kotlin/com/nalepa/mateusz/threadpoolslikeasync/ThreadpoolslikeasyncApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.nalepa.mateusz.threadpoolslikeasync
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ThreadpoolslikeasyncApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
